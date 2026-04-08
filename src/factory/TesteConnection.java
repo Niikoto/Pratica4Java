7f0cfe1f8586243bbd6e5a07b072a74b39b26387
Niikoto <nicolasescobarsouza@gmail.com>
2026-04-08 08:33:30 -0300

    <chore>: Organização dos arquivos

---
+++ src/factory/TesteConnection.java
@@ -0,0 +1,13 @@
+package src.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TesteConnection {
+    public static void main(String[] args)throws SQLException {
+        Connection conection = ConnectionFactory.getConnection();
+        System.out.println("Banco aberto");
+        conection.close();
+        System.out.println("Conecxão fechada");
+    }
+}

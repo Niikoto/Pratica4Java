d992a333d73cfcfb6d78b5ed376a74705321236b
BeteBaltazar <elizabetebaltazar213@gmail.com>
2026-05-13 08:13:15 -0300

    <database>: Criação das tabelas raça e animal

--- src/db/databasePratica4.sql
+++ src/db/databasePratica4.sql
@@ -14,3 +14,28 @@ CREATE TABLE cliente (
 	estado VARCHAR(2),
 	cep VARCHAR(10)
 );
+
+CREATE TABLE raca (
+	id_raca int AUTO_INCREMENT primary key,
+	nome_raca VARCHAR(30) Not null,
+	tipo_animal VARCHAR(30),
+	status_raca TINYINT
+);
+
+CREATE TABLE animal(
+	id_animal int AUTO_INCREMENT primary key,
+	nome_animal VARCHAR(30) not null,
+	data_nascimento DATE,
+	sexo VARCHAR(1),
+	cor VARCHAR(30),
+	observacoes VARCHAR(50),
+	cod_cliente INT not null,
+	cod_raca INT not null,
+	status_animal TINYINT,
+	CONSTRAINT cod_cliente
+		FOREIGN kEY (cod_cliente) 
+		REFERENCES cliente(id),
+	CONSTRAINT cod_raca
+		FOREIGN key (cod_raca)
+		REFERENCES raca(id_raca)
+);
